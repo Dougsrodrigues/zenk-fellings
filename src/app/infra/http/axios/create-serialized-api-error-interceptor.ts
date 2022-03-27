dcf57f8ab080702785c41cceb824157c16d02266
Douglas Santiago Rodrigues <dougsrodrigues@outlook.com>
2022-03-27 12:07:11 -0300

    refactor: remove unused files

--- src/app/infra/http/axios/create-serialized-api-error-interceptor.ts
+++
@@ -1,5 +0,0 @@
-import { createSerializedApiError } from './create-serialized-api-error';
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function createSerializedApiErrorInterceptor(error: any) {
-  return Promise.reject(createSerializedApiError(error));
-}
